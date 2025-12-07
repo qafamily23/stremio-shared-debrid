82c6423cd30b7084517a5f26ed81f27274c1126d
Anh Nguyen <anhkind@gmail.com>
2025-12-07 21:51:35 +0800

    Update manifest.js

--- src/manifest.js
+++ src/manifest.js
@@ -1,11 +1,16 @@
 const packageJson = require('../package.json');
 
 module.exports = {
-  id:           "com.github.anhkind",
-  version:      packageJson.version,
-  name:         "Shared Debrid Notifier",
-  description:  "Notify current user if the shared debrid is being used by others",
-  resources:    ["stream" ],
-  catalogs:     [],
-  types:        ["movie", "series", "channel", "tv"],
+  id:             "com.github.anhkind",
+  version:        packageJson.version,
+  name:           "Shared Debrid Notifier",
+  description:    "Notify current user if the shared debrid is being used by others",
+  logo:           "https://raw.githubusercontent.com/anhkind/stremio-shared-debrid/master/images/logo-monochrome-256.png",
+  resources:      ["stream" ],
+  catalogs:       [],
+  types:          ["movie", "series", "channel", "tv"],
+  behaviorHints:  {
+    configurable:           true,
+    configurationRequired:  true
+  }
 };
